2aea8a8076c6c7c46fccdb609ba6562944a6d480
Miguel Martin <miguel.martin@thefoundry.co.uk>
2016-07-13 11:37:31 +0100

    added more services

--- public/app/components/navbar.component.ts
+++ public/app/components/navbar.component.ts
@@ -8,20 +8,36 @@ import { Page } from '../models/Page';
 import { RegionService } from '../services/region.service';
 import { Region }        from '../models/Region';
 
+// Subjects
+import { SubjectService } from '../services/subject.service';
+import { Subject }       from '../models/Subject';
+
+// Subcategories
+import { SubcategoryService } from '../services/subcategory.service';
+import { Subcategory }        from '../models/Subcategory';
+
 @Component({
     selector: 'navbar',
     templateUrl: '/app/templates/navbar.component.html',
-    providers: [PageService, RegionService]
+    providers: [
+        PageService, 
+        RegionService, 
+        SubjectService, 
+        SubcategoryService]
 })
 
 export class NavbarComponent {
     
     pages: Array<Page>;     // Same as pages: Pages[];
     regions: Array<Region>; // Same as regions: Region[];
+    subjects: Array<Subject>; // Same as subjects: Subjects[];
+    subcategories: Array<Subcategory>; // Same as subcategory: Subcategory[];
     errorMessage: any;
 
     constructor(private _pageService: PageService,
-                private _regionService: RegionService) {
+                private _regionService: RegionService,
+                private _subjectService: SubjectService,
+                private _subcategoryService: SubcategoryService) {
         
     }
     
@@ -36,6 +52,18 @@ export class NavbarComponent {
                            .subscribe(
                                regions => this.regions = regions,
                                error   => this.errorMessage = <any>error
-                           )                         
+                           );
+                           
+        this._subjectService.getTitles()
+                            .subscribe(
+                                subjects => this.subjects = subjects,
+                                error    => this.errorMessage = <any>error
+                            );
+                            
+        this._subcategoryService.getTitles()
+                                .subscribe(
+                                    subcategories => this.subcategories = subcategories,
+                                    error         => this.errorMessage = <any>error 
+                                );                         
     }
 }
