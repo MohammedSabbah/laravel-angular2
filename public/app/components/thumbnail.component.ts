0c25ba7edde21cbbe77b99bea97c15cf2d016701
Miguel Martin <miguel.martin@thefoundry.co.uk>
2016-08-05 15:05:20 +0100

    added some comments

--- public/app/components/thumbnail.component.ts
+++ public/app/components/thumbnail.component.ts
@@ -9,6 +9,21 @@ import { ROUTER_DIRECTIVES } from '@angular/router';
 
 export class ThumbnailComponent {
     
+    // Instead of defining a list of inputs here we can add an 'input'
+    // property to our decorator:
+    //
+    // @Component({ inputs: ['idProfile', 'available'...]})
+    //
+    // We can also define a custom name:
+    // 
+    // @Input('customName') name: string;
+    //  or
+    // @Component({ inputs: ['name:customName'] })
+    //
+    // And in the parent controller we have to do:
+    //
+    // <thumbnail [customName]="value"></thumbnail>
+    
     @Input() idProfile: number;
     @Input() available: string;
     @Input() image: string;
