8de72b93878d133b49ed5656ab04d1ba42b85cf8
Miguel Martin <ermiwe@gmail.com>
2016-09-21 19:48:29 +0100

    clear cache for views

--- public/app/components/index.component.ts
+++ public/app/components/index.component.ts
@@ -8,6 +8,9 @@ import { ThumbnailComponent } from '../components/thumbnail.component';
 
 import { GeoProfilesComponent} from '../components/geo-profiles.component';
 
+// clear the cache so that users see always the latest version
+import { RuntimeCompiler} from '@angular/compiler';
+
 @Component({
     selector: 'index',
     templateUrl: '/app/templates/index.component.html',
@@ -20,11 +23,13 @@ export class IndexComponent {
     thumbnails: Thumbnail;
     errorMessage: any;
 
-    constructor(private _profileService: ProfileService) {
+    constructor(private _profileService: ProfileService,
+                private _runtimeCompiler: RuntimeCompiler) {
 
     }
 
     ngOnInit() {
+        this._runtimeCompiler.clearCache();
         this._profileService.getRandomProfiles()
                             .subscribe(
                                 thumbnails => this.thumbnails = thumbnails,
